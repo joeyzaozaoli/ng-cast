d79f9faa456d51adc9d59dac803a15c5e8453e2d
Joey Li <joeyzaozaoli@gmail.com>
2017-09-08 07:18:51 -0700

    Pass first video of exampleVideoData into videoPlayer component

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,9 @@ angular.module('video-player')
 
   .component('app', {
     controller: function() {
+      console.log(exampleVideoData[0]);
       this.videos = exampleVideoData;
+      this.video = exampleVideoData[0];
     },
     templateUrl: '/src/templates/app.html'
   });
