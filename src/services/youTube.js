661c09b249c6e5eac984f38e6e9ed74823c0b78e
Joey Li <joeyzaozaoli@gmail.com>
2017-09-08 22:12:16 -0700

    Build youTube service using $http

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,22 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
-});
+  .service('youTube', function($http){
+    this.search = function(options, cb) {
+      return $http({
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        method: 'GET',
+        params: {
+          part: 'snippet',
+          maxResults: options.max || 5,
+          q: options.query,
+          type: 'video',
+          videoEmbeddable: 'true',
+          key: options.key
+        }
+      }).then(function successCallback(result) {
+        console.log('get success', result);
+        cb(result.data.items);
+      }, function errorCallback(error) {
+        console.log('get failure', error);
+      });
+    };
+  });
